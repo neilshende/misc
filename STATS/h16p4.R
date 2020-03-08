ae9c78e6ca012628ec186fc5df812657c6a9a543
Vivek Shende <vivek_shende@hotmail.com>
2020-03-07 16:13:23 -0800

    minor edits

--- STATS/h16p4.R
+++ STATS/h16p4.R
@@ -5,12 +5,15 @@ evenind = pidigits$digits[c(seq(2, n, 2))]
 
 t.test(oddind, evenind)
 
-y = c(oddind, evenind)
-N = rep(length(oddind), 2)
+##y = c(oddind, evenind)
+##N = rep(length(oddind), 2)
+y = pidigits$digits
+N = n/2
 group = rep(1:2, N)
 data = data.frame(y=y, group=factor(group))
-fit = lm(y ~ group, data)
-anova(fit)
+###fit = lm(y ~ group, data)
+###anova(fit)
+summary(aov(y ~ group, data))
 
 for (i in 1:25) {
  T = rt(10000, i)
