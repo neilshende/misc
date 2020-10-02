8801d4ef86e64a2e39a35beaaae49baf648be88b
Vivek Shende <vivek_shende@hotmail.com>
2020-10-01 18:03:07 -0700

    add ack

--- a.c
+++ a.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
+int count, partno;
+char *name;
+char *devPart="nvme10n1p1";
+count = sscanf( devPart, "%10[^p]p%d", name, &partno );
 short int x =32767;
 x = x+1;
 x = x+1;
