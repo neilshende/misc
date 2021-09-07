6c0552e22c7298b3a5c7d77293cb5d455c511632
VivekMemverge <vivek.shende@memverge.com>
2021-09-06 17:02:22 -0700

    bash color

---
+++ BASH/color.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+function red() {
+printf "\033[0;31m"
+}
+function green() {
+printf "\033[0;32m"
+}
+function black() {
+printf "\033[0;29m"
+}
+function blue() {
+printf "\033[0;34m"
+}
+function purple() {
+printf "\033[0;35m"
+}
+function skyblue() {
+printf "\033[0;36m"
+}
+function yellow() {
+printf "\033[0;33m"
+}
