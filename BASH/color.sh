d256165e5d24783290e025264de7d736390dbbd3
VivekMemverge <vivek.shende@memverge.com>
2021-09-10 11:31:59 -0700

    echo [0;91m red [0;94m blue [0;92m green

--- BASH/color.sh
+++ BASH/color.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-red=$'\033[0;31m'
-green=$'\033[0;32m'
+red=$'\033[0;91m'
+green=$'\033[0;92m'
 black=$'\033[0;29m'
-blue=$'\033[0;34m'
-purple=$'\033[0;35m'
-skyblue=$'\033[0;36m'
-yellow=$'\033[0;33m'
+blue=$'\033[0;94m'
+purple=$'\033[0;95m'
+skyblue=$'\033[0;96m'
+yellow=$'\033[0;93m'
 
 function red() {
-printf "\033[0;31m"
+printf "\033[0;91m"
 }
 function green() {
-printf "\033[0;32m"
+printf "\033[0;92m"
 }
 function black() {
 printf "\033[0;29m"
 }
 function blue() {
-printf "\033[0;34m"
+printf "\033[0;94m"
 }
 function purple() {
-printf "\033[0;35m"
+printf "\033[0;95m"
 }
 function skyblue() {
-printf "\033[0;36m"
+printf "\033[0;96m"
 }
 function yellow() {
-printf "\033[0;33m"
+printf "\033[0;93m"
 }
