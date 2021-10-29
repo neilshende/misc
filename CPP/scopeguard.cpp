5cc13fafa25ace5ef9ab4e974b625b92990af1a1
VivekMemverge <vivek.shende@memverge.com>
2021-10-28 19:59:10 -0700

    implement scope grd

--- CPP/scopeguard.cpp
+++ CPP/scopeguard.cpp
@@ -1,6 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "scope_guard.h"
+//#include "scope_guard.h"
+#include <functional>
+
+class ScopeGuard {
+ public:
+  explicit ScopeGuard(std::function<void()> fn) : fn_(fn) {}
+  ~ScopeGuard() {
+    if (!dismissed_) fn_();
+  }
+
+  ScopeGuard &operator=(const ScopeGuard &) = delete;
+  ScopeGuard &operator=(ScopeGuard &&) = delete;
+
+  void Dismiss() { dismissed_ = true; }
+
+  void Execute() {
+    if (!dismissed_) {
+      dismissed_ = true;
+      fn_();
+    }
+  }
+
+ private:
+  std::function<void()> fn_;
+  bool dismissed_ = false;
+};
 
 int main(int argc, char *argv[]) {
 
