7073dbd60e2ae5e1a5f2c9323dc03fdb56b0f42d
VivekMemverge <vivek.shende@memverge.com>
2022-01-11 16:16:30 -0800

    system_grep

---
+++ LINUX/sysgrep.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <iostream>
+int main(int argc, char *argv[]) {
+std::string log_file_;
+if (argc > 1 ) {
+   log_file_ = argv[1];
+} else {
+   log_file_ = "test";
+}
+std::string cmd = std::string("tail -n 10 ") + log_file_ +
+   " | grep -e 'Pid.*do not match expected' -e 'Unable to create a thread' >/dev/null 2>&1";
+
+if (std::system(cmd.c_str())) {
+   std::cout << "system return non zero\n";
+} else {
+   std::cout << "system return zero\n";
+}
+return 0;
+}
+
