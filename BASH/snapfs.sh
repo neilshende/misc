d52b825a106018c3603968ea4e4438107c6279eb
VivekMemverge <vivek.shende@memverge.com>
2022-03-26 12:59:05 -0700

    error check

--- BASH/snapfs.sh
+++ BASH/snapfs.sh
@@ -10,13 +10,26 @@ function dirslash()
 
 function list_open_files_for_write()
 {
-  local -a PIDS=$(pstree $1 -Tpa | awk 'BEGIN {FS=","} {print $2}' | awk '{print $1}')
+  local -a PIDS=$(set -o pipefail; pstree $1 -Tpa | awk 'BEGIN {FS=","} {print $2}' | awk '{print $1}')
+  if [ $? != 0 ];then
+     >&2 echo $MY_NAME Failed read pstree of $1 .
+     exit 1
+  fi
   for PID in ${PIDS[@]} ; do
     >&2 echo PID is $PID
     for f in /proc/${PID}/fd/* ;do
-      if [ -f $(readlink $f | awk '{print $1}') ]; then
+      local openfile=$(set -o pipefail; readlink $f | awk '{print $1}')
+      if [ $? != 0 ];then
+         >&2 echo $MY_NAME Failed read open file of $1 .
+         exit 1
+      fi
+      if [ -f $(openfile) ]; then
         local fb=$(basename $f)
-        local flag=$(cat /proc/${PID}/fdinfo/${fb} | awk '/flags:/{print $2}')
+        local flag=$(set -o pipefail; cat /proc/${PID}/fdinfo/${fb} | awk '/flags:/{print $2}')
+        if [ $? != 0 ];then
+          >&2 echo $MY_NAME Failed read fdinfo of ${PID} .
+          exit 1
+        fi
         local wrrd=$((flag&3))
           >&2 echo  $(readlink $f) : WRRD is $wrrd FLAG is $flag
         if [[ $wrrd != 0 ]]; then
