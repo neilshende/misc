306718ded4e50809c41e4aeaddf032b9f435828d
Vivek Shende <vivek_shende@hotmail.com>
2022-06-06 18:33:00 -0700

    all_of

---
+++ STL/all_of.cpp
@@ -0,0 +1,24 @@
+// all_of
+// any_of none_of for_each
+// work similarly.
+#include <iostream>     // std::cout
+#include <algorithm>    // std::all_of
+#include <array>        // std::array
+bool is_odd(int i) {
+   return i%2;
+}
+struct myclass {           // function object type:
+  void operator() (int i) {std::cout << ' ' << i;}
+} functor;
+
+int main () {
+  std::array<int,8> foo = {3,5,7,11,13,17,19,23};
+
+  //if ( std::all_of(foo.begin(), foo.end(), [](int i){return i%2;}) )
+  if ( std::all_of(foo.begin(), foo.end(), is_odd) )
+    std::cout << "All the elements are odd numbers.\n";
+
+  std::for_each(foo.begin(), foo.end(), functor);
+
+  return 0;
+}
