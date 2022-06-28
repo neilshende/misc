53f5c6a7e70bcc65de72914089e21115d20e8783
Vivek Shende <vivek_shende@hotmail.com>
2022-06-28 13:56:46 -0700

    kth

--- bst.c
+++ bst.c
@@ -157,6 +157,43 @@ node * pred(struct node *head, struct node *curr)
    return NULL;
 }
 
+// A function to find k'th largest element in a given tree.
+void kthLargestUtil(node *root, int k, int &c)
+{
+   // Base cases, the second condition is important to
+   // avoid unnecessary recursive calls
+   if (root == NULL || c >= k)
+      return;
+
+   // Follow reverse inorder traversal so that the
+   // largest element is visited first
+   kthLargestUtil(root->right, k, c);
+
+   // Increment count of visited nodes
+   c++;
+
+   // If c becomes k now, then this is the k'th largest
+   if (c == k)
+   {
+      cout << "K'th largest element is "
+         << root->value << endl;
+      return;
+   }
+
+   // Recur for left subtree
+   kthLargestUtil(root->left, k, c);
+}
+
+// Function to find k'th largest element
+void kthLargest(node *root, int k)
+{
+   // Initialize count of nodes visited as 0
+   int c = 0;
+
+   // Note that c is passed by reference
+   kthLargestUtil(root, k, c);
+}
+
 // This function finds predecessor and successor of key in BST.
 // It sets pre and suc as predecessor and successor respectively
 void findPreSuc(node * root, node *& pre, node *& suc, int value)
