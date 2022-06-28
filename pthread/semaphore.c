ba99e80edd6caa3c518d345b198b8128667aa3a2
Vivek Shende <vivek_shende@hotmail.com>
2022-06-28 11:57:09 -0700

    consumer producer

--- pthread/semaphore.c
+++ pthread/semaphore.c
@@ -1,4 +1,23 @@
 #include <semaphore.h>
+//producer-consumer using semaphore.
+
+typedef struct {
+    char buf[BSIZE];
+    sem_t occupied;
+    sem_t empty;
+    int nextin;
+    int nextout;
+    sem_t pmut;
+    sem_t cmut;
+} buffer_t;
+
+buffer_t buffer;
+
+sem_init(&buffer.occupied, 0, 0);
+sem_init(&buffer.empty,0, BSIZE);
+sem_init(&buffer.pmut, 0, 1);
+sem_init(&buffer.cmut, 0, 1);
+buffer.nextin = buffer.nextout = 0;
 
 void producer(buffer_t *b, char item) {
     sem_wait(&b->empty);
