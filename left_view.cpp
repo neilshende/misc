0488d78fb973072f8cacb62aeff53c4e1eec5017
Vivek Shende <vivek_shende@hotmail.com>
2022-07-01 21:06:19 -0700

    another impl.

--- left_view.cpp
+++ left_view.cpp
@@ -38,8 +38,53 @@ void leftView(Node* root, int level, int &last_level)
 }
 
 // Function to print the left view of a given binary tree
-void leftView(Node* root)
+void leftViewRecursive(Node* root)
 {
     int last_level = 0;
     leftView(root, 1, last_level);
+}
+
+// Iterative function to print the left view of a given binary tree
+void leftViewIterative(Node* root)
+{
+    // return if the tree is empty
+    if (root == nullptr) {
+        return;
+    }
+
+    // create an empty queue and enqueue the root node
+    list<Node*> queue;
+    queue.push_back(root);
+
+    // pointer to store the current node
+    Node* curr = nullptr;
+
+    // loop till queue is empty
+    while (!queue.empty())
+    {
+        // calculate the total number of nodes at the current level
+        int size = queue.size();
+        int i = 0;
+
+        // process every node of the current level and enqueue their
+        // non-empty left and right child
+        while (i++ < size)
+        {
+            curr = queue.front();
+            queue.pop_front();
+
+            // if this is the first node of the current level, print it
+            if (i == 1) {
+                cout << curr->key << " ";
+            }
+
+            if (curr->left) {
+                queue.push_back(curr->left);
+            }
+
+            if (curr->right) {
+                queue.push_back(curr->right);
+            }
+        }
+    }
 }
