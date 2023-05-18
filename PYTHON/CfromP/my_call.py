f999743c17e5f341c013498a06ef94664c2f7709
Vivek Shende <vivek_shende@hotmail.com>
2023-05-18 09:38:45 -0700

    struct, sys

--- PYTHON/CfromP/my_call.py
+++ PYTHON/CfromP/my_call.py
@@ -1,4 +1,9 @@
 from ctypes import *
+import struct
+import sys
 so_file = "./my_functions.so"
 my_functions = CDLL(so_file)
 print(my_functions.square(10))
+#hopefully we don't need to mess with:
+sys.byteorder
+struct.pack("hil",1,2,3) #half, int, long
