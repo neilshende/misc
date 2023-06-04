46e3df24f974b8532e64ce19f318abd4856a829b
Vivek Shende <vivek_shende@hotmail.com>
2023-06-03 21:37:58 -0700

    minor improvements

--- GRPC/pb2cpp.sh
+++ GRPC/pb2cpp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cat $1 |awk -f proto.awk | tr -d "()" | sed 's/\[/(/g' | sed 's/\]/)/g' >$2
-source $2
-cat async_copy_script.sh >>$2
-chmod a+x $2
-./$2 > $3
+tmp=./tmp_intermidiate_script.sh
+cat $1 | awk -f proto.awk | tr -d "()" | sed 's/\[/(/g' | sed 's/\]/)/g' >$tmp
+cat async_copy_script.sh >>$tmp
+chmod a+x $tmp
+$($tmp > $2)
