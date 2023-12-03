4dd70dc397f172f703273eab240e95c77cc50afa
Vivek Shende <vivek_shende@hotmail.com>
2023-12-03 15:53:58 -0800

    ps

--- PYTHON/ps.py
+++ PYTHON/ps.py
@@ -1,13 +1,20 @@
 import psutil
 
+def graceful(process_id):
+   process = psutil.Process(process_id)
+   process.send_signal(psutil.SIGTERM)
+
+def killgroup(process_id):
+   process = psutil.Process(process_id)
+   for child in process.children(recursive=True):
+       child.kill()
+   process.kill()
+
+def pkill(pname):
+    processes = psutil.process_iter()
+    for process in processes:
+       if process.name() == pname:
+            process.kill()
+
 p = psutil.Popen(["python", "-c", "print 'hi'"])
-
-# Get all processes
-processes = psutil.process_iter()
-
-# Kill all processes with the name "python"
-for process in processes:
-    if process.name() == "python":
-        process.kill()
-
 p.wait()
