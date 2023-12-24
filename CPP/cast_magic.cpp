37204c2e846887d2babb71ff2e03fa6ceda56742
Vivek Shende <vivek_shende@hotmail.com>
2023-12-24 14:05:38 -0800

    cast magic

--- CPP/cast_magic.cpp
+++ CPP/cast_magic.cpp
@@ -10,18 +10,19 @@ public:
   hello() { magic = 911911;}
   ~hello() { magic = 199199;}
   int getmagic() { return magic;}
+  bool valid() { return magic==911911;}
 };
 int main(int argc, char *argv[]) {
    hello *h = new hello;
    long c = (long)h;
    hello *hc = (hello *)c;
-   cout << "c style cast " << hc->getmagic() << endl;
+   cout << "c style cast " << hc->getmagic() << " valid=" << hc->valid() << endl;
    long p = reinterpret_cast<long>(h);
-   cout << "c++ cast " << reinterpret_cast<hello *>(p)->getmagic() << endl;
+   cout << "c++ cast " << reinterpret_cast<hello *>(p)->getmagic() << " valid=" << reinterpret_cast<hello *>(p)->valid() << endl;
    hello *h2 = reinterpret_cast<hello *>(p+100);
-   cout << (h2->getmagic()==911911? "good" : "bad") << endl;
+   cout << h2->getmagic()<< " valid=" << h2->valid() <<endl;
    h2->print();
    delete h;
-   cout <<  reinterpret_cast<hello *>(p)->getmagic() << endl;
+   cout <<  reinterpret_cast<hello *>(p)->getmagic() << " valid=" << reinterpret_cast<hello *>(p)->valid() << endl;
    return 0;
 }
