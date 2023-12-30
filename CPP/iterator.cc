5b1de89f5e3342d68ffd6c5c653bcd83fb2f8863
Vivek Shende <vivek_shende@hotmail.com>
2023-12-30 08:59:06 -0800

    implement container and its iterator

---
+++ CPP/iterator.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iterator>
+
+// Sample class to iterate over
+template <typename T>
+class MyClass {
+public:
+    T *data;
+    int size;
+
+    MyClass(const int max): size(max) { data = new T[size]; }
+    ~MyClass() { delete [] data;}
+
+    // Define the iterator type as a nested class
+    class iterator : public std::iterator<std::forward_iterator_tag, T> {
+    public:
+        iterator(T *it) : current(it) {}
+
+        int& operator*() const { return *current; }
+        iterator& operator++() { ++current; return *this; }
+        bool operator==(const iterator& other) const { return current == other.current; }
+        bool operator!=(const iterator& other) const { return !(*this == other); }
+        bool operator<(const iterator& other) const { return current < other.current; }
+
+    private:
+        T * current;
+    };
+
+    // Begin and end iterators for the class
+    iterator begin() { return (&data[0]); }
+    iterator end() { return (&data[size]); }
+};
+
+int main() {
+    MyClass<int> myClass(100);
+
+    // Use iterators directly for more control
+    int i = 0;
+    for (MyClass<int>::iterator it = myClass.begin(); it < myClass.end(); ++it) {
+        *it = i++;  // Modify the values through the iterator
+    }
+
+    for (int value : myClass) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+}
