ea5542a4dc238cab608528eedd5082e40c80531d
Vivek Shende <vivek_shende@hotmail.com>
2023-12-30 11:58:55 -0800

    add copy constructor and assignment operator

--- CPP/iterator.cc
+++ CPP/iterator.cc
@@ -19,6 +19,7 @@ public:
 
         int& operator*() const { return *current; }
         iterator& operator++() { ++current; return *this; }
+        iterator& operator++(int) {iterator t=*this; ++current; return t; }
         bool operator==(const iterator& other) const { return current == other.current; }
         bool operator!=(const iterator& other) const { return !(*this == other); }
         bool operator<(const iterator& other) const { return current < other.current; }
@@ -30,6 +31,39 @@ public:
     // Begin and end iterators for the class
     iterator begin() { return iterator(&data[0]); }
     iterator end() { return iterator(&data[size]); }
+    // Copy constructor
+    MyClass(MyClass& other) {
+       size = other.size;
+       data = new T[size];
+       // copy out all the elements using the iterators.
+       iterator it = begin();
+       for(T value : other) {
+          *it = value;
+          it++;
+       }
+    }
+    // Assignment operator
+    MyClass& operator=(MyClass& other) {
+        if (this != &other) {  // Check for self-assignment
+            delete [] data;
+            size = other.size;
+            data = new T[size];
+            iterator it = begin();
+            for(T value : other) {
+               *it = value;
+               it++;
+            }
+        }
+        return *this;
+    }
+    // Overloaded operator<< for MyClass
+    friend std::ostream& operator<<(std::ostream& os, MyClass& obj) {
+        for (T value : obj) {
+           os << value << " ";
+        }
+        os << "\n";
+        return os;
+    }
 };
 
 int main() {
@@ -37,7 +71,7 @@ int main() {
 
     // Use iterators directly for more control
     int i = 0;
-    for (MyClass<int>::iterator it = myClass.begin(); it < myClass.end(); ++it) {
+    for (MyClass<int>::iterator it = myClass.begin(); it < myClass.end(); it++) {
         *it = i++;  // Modify the values through the iterator
     }
 
@@ -45,4 +79,20 @@ int main() {
         std::cout << value << " ";
     }
     std::cout << std::endl;
+
+    MyClass<int> yourClass(myClass);
+    for (int yvalue : yourClass) {
+       std::cout << yvalue << " ";
+    }
+    std::cout << std::endl;
+
+
+    MyClass<int> copyClass(50);
+    copyClass = myClass;
+    for (int cvalue : copyClass) {
+       std::cout << cvalue << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << copyClass;
 }
