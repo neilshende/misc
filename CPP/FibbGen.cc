4606ab6d2dfc6cf587b6823ab2f8ecd04f2cda35
Vivek Shende <vivek_shende@hotmail.com>
2023-12-31 14:07:31 -0800

    user generator to find nth

--- CPP/FibbGen.cc
+++ CPP/FibbGen.cc
@@ -23,13 +23,26 @@ long FibGen() {
   b=r+b;
   return r;
 }
+
+long Fibonacci(int n) {
+   if (n<1 || n>92) return -1;
+   fsg F;
+   long r;
+   while(n--) {
+     r = F();
+   }
+   return r;
+}
 int main() {
 	fsg FS;
+	int i = 1;
 	for (auto f = FS(); f>0; f = FS()) {
-		std::cout << f << std::endl;
+		std::cout << i++  << " : " << f << std::endl;
 	}
+	i = 1;
 	for (auto f = FibGen(); f>0; f = FibGen()) {
-		std::cout << f << std::endl;
+		std::cout << i++  << " : " << f << std::endl;
 	}
+	std::cout << "91 : " << Fibonacci(91) << std::endl;
 	return 0;
 }
