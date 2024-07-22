629d4c420aabca8bf117625d54b18aa09244d3ef
Vivek Shende <vivek_shende@hotmail.com>
2024-07-21 20:05:35 -0700

    fastq preocessing

---
+++ PYTHON/make_fastqR3.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import sys
+import gzip
+
+
+Read1 = sys.argv[1]
+Read3 = sys.argv[2]
+out4 = b"IIIIIIIIII\n"
+file1 = gzip.open(Read1)
+f3 = gzip.open(Read3, 'wb')
+line1 = file1.readline()
+
+while line1:
+    out2 = bytes(((str(line1).split(' ')[1]).split(',')[0]) + "\n", encoding = 'utf-8')
+    line2 = file1.readline()
+    line3 = file1.readline()
+    line4 = file1.readline()
+    f3.write(line1)
+    f3.write(out2)
+    f3.write(line3)
+    f3.write(out4)
+    line1 = file1.readline()
+
+file1.close()
+f3.close()
