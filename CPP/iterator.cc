b380de00ace094526f4d213c577569fc13911182
Vivek Shende <vivek_shende@hotmail.com>
2025-01-15 14:35:49 -0800

    minor

--- CPP/iterator.cc
+++ CPP/iterator.cc
@@ -20,7 +20,7 @@ public:
         iterator(T *it) : current(it) {}
 
         T& operator*() const { return *current; }
-        iterator operator++() { ++current; return *this; }
+        iterator& operator++() { ++current; return *this; }
         iterator operator++(int) {iterator t(*this); ++current; return t; }
         bool operator==(const iterator& other) const { return current == other.current; }
         bool operator!=(const iterator& other) const { return !(*this == other); }
